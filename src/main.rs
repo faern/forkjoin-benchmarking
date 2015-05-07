effd0635606344fa3efb38b15b4440137b173b09
Linus Färnstrand <faern@faern.net>
2015-05-07 11:22:54 +0200

    Cleaned up a little bit. All benchs are running

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(std_misc)]
+//#![feature(std_misc)] // For Duration
 
 extern crate criterion;
 extern crate forkjoin;
@@ -6,9 +6,10 @@ extern crate forkjoin;
 mod fib;
 mod quicksort;
 
-use std::time::duration::Duration;
-use std::env;
+//use std::time::duration::Duration;
+
 use criterion::Criterion;
+
 use fib::{parfib_t1, parfib_t4, parfib_i30, seqfib};
 use quicksort::{seq_qsort_sorted, par_qsort_t1_sorted, par_qsort_t4_sorted};
 use quicksort::{seq_qsort_rnd, par_qsort_t1_rnd, par_qsort_t4_rnd};
@@ -16,14 +17,14 @@ use quicksort::{seq_qsort_rnd, par_qsort_t1_rnd, par_qsort_t4_rnd};
 fn main() {
     let threads = vec![1,2,3,4,5];
     let fibdata = vec![20, 25, 30, 35];
-
     let qsortdata = vec![1, 1000, 5000, 10000];
 
+
     Criterion::default().sample_size(20)
-        // .bench_with_inputs("seqfib", seqfib, fibdata.clone())
-        // .bench_with_inputs("parfib_t1", parfib_t1, fibdata.clone())
-        // .bench_with_inputs("parfib_t4", parfib_t4, fibdata.clone())
-        // .bench_with_inputs("parfib_i30", parfib_i30, threads.clone())
+        .bench_with_inputs("seqfib", seqfib, fibdata.clone())
+        .bench_with_inputs("parfib_t1", parfib_t1, fibdata.clone())
+        .bench_with_inputs("parfib_t4", parfib_t4, fibdata.clone())
+        .bench_with_inputs("parfib_i30", parfib_i30, threads.clone())
 
         .bench_with_inputs("seq_qsort_sorted", seq_qsort_sorted, qsortdata.clone())
         .bench_with_inputs("par_qsort_t1_sorted", par_qsort_t1_sorted, qsortdata.clone())
